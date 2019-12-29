71ee61264dd2ef86950bbd7d711bc3c3f25cf8f7
Maksim Ivanov <satansdeer@gmail.com>
2019-12-29 02:44:17 +0100

    Add final changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
 
-function App() {
+const categories = [
+  { id: 0, title: "Kingdom: Animal", subcategories: [1, 2, 3, 4] },
+  { id: 1, title: "Phylum: Chordata", subcategories: [2] },
+  { id: 2, title: "Clade: Synapsida", subcategories: [3] },
+  { id: 3, title: "Class: Mammailia", subcategories: [4] },
+  { id: 4, title: "Order: Carnivora", subcategories: [5] },
+  { id: 5, title: "Subfamily: Felinae", subcategories: [6] },
+  { id: 6, title: "Genus: Felis", subcategories: [7] },
+  { id: 7, title: "Species: Felis catus", subcategories: [] }
+];
+
+const Category = ({ match }) => {
+  const category = categories.find(category => {
+    return parseInt(match.params.catId) === category.id;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {match.isExact && (
+        <>
+          <h1>{category.title}</h1>
+          {category.subcategories.map(subCategoryId => {
+            return (
+              <div>
+                <Link key={subCategoryId} to={`${match.url}/${subCategoryId}`}>
+                  {categories.find(cat => cat.id === subCategoryId).title}
+                </Link>
+              </div>
+            );
+          })}
+          {category.id === 7 && (
+            <img src="https://cataas.com/cat" alt="cat" height="300" />
+          )}
+        </>
+      )}
+      <Switch>
+        <Route path={`${match.path}/:catId`} component={Category} />
+      </Switch>
+    </>
   );
-}
+};
 
-export default App;
+export const RoutingExample = () => {
+  return (
+    <Switch>
+      <Route path={`/:catId`} component={Category} />
+    </Switch>
+  );
+};
